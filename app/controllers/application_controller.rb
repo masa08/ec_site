82bc70c8a00a8f5270be8de751dfbba9691ae92c
Masa08 <ushimasarug08@gmail.com>
2018-03-09 08:30:46 +0000

    change model about Cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_cart
+
+  def current_cart
+     if session[:cart_id]
+       @cart = Cart.find(session[:cart_id])
+     else
+       @cart = Cart.create
+       session[:cart_id] = @cart.id
+     end
+  end
+
 end
