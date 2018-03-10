53ff31d0379a083eff2474b6d35baebf8da0f05e
小野瀬萌 <onosemoe@onomoe.local>
2018-03-10 18:33:41 +0900

    user show edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
   protect_from_forgery with: :exception
 
   helper_method :current_cart
@@ -12,4 +15,10 @@ class ApplicationController < ActionController::Base
      end
   end
 
+  protected
+
+  def configure_permitted_parameters
+    #strong parametersを設定し、usernameを許可
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:user_name_kanji, :postal_code, :phone_number, :email, :address, :user_name_kana, :password, :password_confirmation) }
+  end
 end
