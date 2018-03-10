47c9a5fa3f40ef0767b2120594697dc2662b309c
Masa08 <ushimasarug08@gmail.com>
2018-03-10 11:43:17 +0000

    Changed ransack

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :ransack, only: [:index, :show]
+
+
   def index
     @items = Item.all
     @item = Item.new
-    # ransack
-    @search = Item.ransack(params[:q])  #追加
-    @results = @search.result
-  end
 
-  def admin_index
-    @items = Item.all
-    @item = Item.new
   end
 
   def show
     @item = Item.find(params[:id])
   end
 
-  def admin_show
-    @item = Item.find(params[:id])
-  end
-
   def new
-    @item = Item.new
-  end
-
-  def admin_new
     @item = Item.new
   end
 
@@ -33,9 +21,7 @@ class ItemsController < ApplicationController
     item.save
     redirect_to item_path(item.id)
   end
-  def admin_edit
 
-  end
   def update
 
   end
@@ -48,6 +34,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :stock, :artist_name,
       :jacket_image, :price, :label, :item_status_id, :release_date_id)
+  end
+
+  def ransack
+    # ransack
+    @search = Item.ransack(params[:q])  #追加
+    @results = @search.result
   end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,4 @@
 class Cart < ApplicationRecord
     has_many :item_carts
-
+    belongs_to :user
 end
