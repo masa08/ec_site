a52cecd23cb09da95f2d7cc8897f583f9a865cbf
Masa08 <ushimasarug08@gmail.com>
2018-03-10 12:43:43 +0000

    change user

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,37 +1,19 @@
 class CartsController < ApplicationController
-  before_action :setup_cart_item!, only: [:add_item, :update_item, :delete_item]
-
   def show
-    @cart_items = current_cart.cart_items
-  end
 
+  end
   # 商品一覧画面から、「商品購入」を押した時のアクション
   def add_item
-    if @cart_item.blank?
-      @cart_item = current_cart.cart_items.build(product_id: params[:product_id])
-    end
 
-    @cart_item.quantity += params[:quantity].to_i
-    @cart_item.save
-    redirect_to current_cart
   end
 
 # カート詳細画面から、「更新」を押した時のアクション
   def update_item
-    @cart_item.update(quantity: params[:quantity].to_i)
-    redirect_to current_cart
-  end
 
+  end
 # カート詳細画面から、「削除」を押した時のアクション
   def delete_item
-    @cart_item.destroy
-    redirect_to current_cart
-  end
 
-private
-
-  def setup_cart_item!
-    @cart_item = current_cart.cart_items.find_by(product_id: params[:product_id])
   end
 
 end
