06348afdd782ef2ac6196807e7793f01cbb351ed
Masa08 <ushimasarug08@gmail.com>
2018-03-11 07:59:29 +0000

    cart basic complete

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,24 @@
 class CartsController < ApplicationController
   def show
-
+    @item_carts = ItemCart.all
   end
   # 商品一覧画面から、「商品購入」を押した時のアクション
   def add_item
     @item = params[:item_id]
-    binding.pry
-
-
+    # Create Cart
+    if session[:cart_id]
+      @cart = Cart.find(session[:cart_id])
+    else
+      @cart = Cart.create
+      @cart.user_id = current_user.id
+      session[:cart_id] = @cart.id
+      @cart.save
+    end
+    # take cart_id
+    @cart_id = @cart.id
+    @item_cart = ItemCart.new(item_id: @item, cart_id: @cart_id)
+    @item_cart.save
+    redirect_to cart_path(@cart_id)
   end
 
 # カート詳細画面から、「更新」を押した時のアクション
