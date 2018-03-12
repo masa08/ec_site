d73bb234d240096343cc9815c52f8c81ee397a15
Masa08 <ushimasarug08@gmail.com>
2018-03-12 11:18:12 +0000

    Cart add complete

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,32 +2,39 @@ class CartsController < ApplicationController
   def show
     @item_carts = ItemCart.all
   end
-  # 商品一覧画面から、「商品購入」を押した時のアクション
+
   def add_item
+    # get item.id
     @item = params[:item_id]
-    # Create Cart
-    if session[:cart_id]
-      @cart = Cart.find(session[:cart_id])
+    # Create Cart or Find Cart
+    if Cart.where(user_id: current_user.id).presence
+      @cart = Cart.find_by(user_id: current_user.id)
     else
-      @cart = Cart.create
+      @cart = Cart.new
       @cart.user_id = current_user.id
-      session[:cart_id] = @cart.id
       @cart.save
     end
     # take cart_id
     @cart_id = @cart.id
-    @item_cart = ItemCart.new(item_id: @item, cart_id: @cart_id)
-    @item_cart.save
+    # get all ids
+
+    # Create ItemCart
+    if ItemCart.where(cart_id: @cart_id, item_id: @item).presence
+      @item_cart = ItemCart.find_by(cart_id: @cart_id, item_id: @item)
+      @item_cart.item_count += 1
+      @item_cart.update(item_count: @item_cart.item_count)
+    else
+      @item_cart = ItemCart.new(item_id: @item, cart_id: @cart_id, item_count: 1)
+      @item_cart.save
+    end
+
     redirect_to cart_path(@cart_id)
   end
 
-# カート詳細画面から、「更新」を押した時のアクション
   def update_item
 
   end
-# カート詳細画面から、「削除」を押した時のアクション
   def delete_item
 
   end
-
 end
