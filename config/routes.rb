44f88190bef4ef422dbc6fba7877849d1a5e5218
Masa08 <ushimasarug08@gmail.com>
2018-03-13 03:15:15 +0000

    ItemCart complete

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ resources :carts, only: [:show]
 
   post '/add_item' => 'carts#add_item'
   post '/update_item' => 'carts#update_item'
-  delete '/delete_item' => 'carts#delete_item'
+  post '/delete_item' => 'carts#delete_item'
 
 
 post 'purchases/comfirmation' => 'purchases#comfirmation'
