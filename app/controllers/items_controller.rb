d9f7a49e39b6e9bdd236a383bce6921aa79c9c88
小野瀬萌 <onosemoe@onomoe.local>
2018-03-17 13:36:52 +0900

    seed fuファイル除外

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   def index
     # 小野瀬サイドバー検証中
     if params[:types_id].present?
-      # binding.pry
       @items = Item.where(types_id: params[:types_id])
     else
       @items = Item.all
@@ -34,7 +33,7 @@ class ItemsController < ApplicationController
   end
 
   private
-  
+
   def item_params
     params.require(:item).permit(:item_name, :stock, :artist_name,
       :jacket_image, :price, :label, :item_status_id, :release_date_id)
