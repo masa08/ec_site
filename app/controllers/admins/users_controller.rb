9f41f081061f47f71e7edb3db3bd3263b45f014a
平山大稀 <hirayamadaiki@hirayamadaimarenoMacBook-Pro.local>
2018-03-21 12:16:32 +0900

    add user view

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,14 +1,20 @@
 class Admins::UsersController < ApplicationController
 	def index
-		
+		@users = User.all
 	end
 	def show
-		
+		@user = User.find(params[:id])
 	end
 	def edit
-		
+		@user = User.find(params[:id])
 	end
 	def update
-		
+		user = User.find(params[:id])
+		user.update(params[:id])
+		redirect_to admins_users_path
+	end
+	private
+	def user_params
+		params.require(:user).permit(:email, :user_name_kanji, :user_name_kana, :address, :postal_code, :phone_number)
 	end
 end
