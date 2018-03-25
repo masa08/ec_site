b7124485e32d13980fa2549ddaa6bd544a98b580
平山大稀 <hirayamadaiki@hirayamadaimarenoMacBook-Pro.local>
2018-03-25 12:50:08 +0900

    no conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322104914) do
+ActiveRecord::Schema.define(version: 20180324071221) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20180322104914) do
     t.integer "release_date_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "types_id"
+    t.string "type_id"
   end
 
   create_table "payments", force: :cascade do |t|
