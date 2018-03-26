09145ae58b4c42b1a67e064b0d76e26cf6064c84
平山大稀 <hirayamadaiki@hirayamadaimarenoMacBook-Pro.local>
2018-03-26 17:18:18 +0900

    a

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180326033959) do
 
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
